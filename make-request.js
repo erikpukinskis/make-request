2095bf4198a01b79499ccde07ef0d2a131bdd95f
Erik Pukinskis <erik@snowedin.net>
2019-11-16 15:58:41 -0800

    oops

--- make-request.js
+++ make-request.js
@@ -227,7 +227,7 @@ module.exports = library.export(
           }
           log(message, "←", options.method, params.url)
         } else {
-          response.statusCode && log(response.statusCode.toString(), require("http").STATUS_CODES[response.statusCode], "←", options.method, params.url)
+          response && log(response.statusCode.toString(), require("http").STATUS_CODES[response.statusCode], "←", options.method, params.url)
         }
 
         var content = response && response.body
